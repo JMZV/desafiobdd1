f4a2d2d02b95f64bb1ed1f4279e52013996b02c2
Jose Zepeda <jose.zepeda@outlook.com>
2020-09-18 22:44:04 -0300

    first commit postgres desafio part two is beginnig

---
+++ desafio2.sql
@@ -0,0 +1,17 @@
+--1. Crear base de datos llamada películas
+
+--2. Revisar los archivos peliculas.csv y reparto.csv para crear las tablas correspondientes, determinando la relación entre ambas tablas.
+
+--3. Cargar ambos archivos a su tabla correspondiente 
+
+--4. Listar todos los actores que aparecen en la película "Titanic", indicando el título de la película, año de estreno, director y todo el reparto. 
+
+--5. Listar los titulos de las películas donde actúe Harrison Ford.
+
+--6. Listar los 10 directores mas populares, indicando su nombre y cuántas películas aparecen en el top 100.
+
+--7. Indicar cuantos actores distintos hay 
+
+--8. Indicar las películas estrenadas entre los años 1990 y 1999 (ambos incluidos) ordenadas por título de manera ascendente.
+
+--9. Listar el reparto de las películas lanzadas el año 2001
